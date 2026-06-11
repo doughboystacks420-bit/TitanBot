375ef06987490aecb863361c5f9a588d481fce17
doughboystacks420-bit <doughboystacks420@gmail.com>
2026-06-10 20:27:51 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "drugs and no sleep",
+        name: "hard work and no sleep",
         // Activity type number (0 = Playing).
         type: 4, 
       },
